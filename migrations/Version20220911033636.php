c5c8f04fff1c45e236a27f643086e98dcc8bf8ae
ujikstark <ujikbauk@gmail.com>
2022-09-18 11:00:55 +0700

    change migrations postgre

--- migrations/Version20220911033636.php
+++ migrations/Version20220911033636.php
@@ -20,7 +20,7 @@ final class Version20220911033636 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE app_user (id SERIAL NOT NULL, name VARCHAR(180) NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(255) NOT NULL, birth_date DATETIME DEFAULT NULL, gender TINYINT(1) DEFAULT NULL, address LONGTEXT DEFAULT NULL, phone_number VARCHAR(20) DEFAULT NULL, status VARCHAR(50) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, created_at DATETIME NOT NULL, ended_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE app_user (id SERIAL NOT NULL, name VARCHAR(180) NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(255) NOT NULL, birth_date DATETIME DEFAULT NULL, gender TINYINT(1) DEFAULT NULL, address LONGTEXT DEFAULT NULL, phone_number VARCHAR(20) DEFAULT NULL, status VARCHAR(50) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, created_at DATETIME NOT NULL, ended_at DATETIME DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
