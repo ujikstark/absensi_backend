72cf15f300c9d2d953c0abd866e510985a4d24dc
ujikstark <ujikbauk@gmail.com>
2022-09-18 11:58:44 +0700

    change gender type to boolean

--- migrations/Version20220911033636.php
+++ migrations/Version20220911033636.php
@@ -20,7 +20,7 @@ final class Version20220911033636 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE app_user (id SERIAL NOT NULL, name VARCHAR(180) NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(255) NOT NULL, birth_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, gender TINYINT(1) DEFAULT NULL, address LONGTEXT DEFAULT NULL, phone_number VARCHAR(20) DEFAULT NULL, status VARCHAR(50) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, ended_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE app_user (id SERIAL NOT NULL, name VARCHAR(180) NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(255) NOT NULL, birth_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, gender BOOLEAN NULL, address LONGTEXT DEFAULT NULL, phone_number VARCHAR(20) DEFAULT NULL, status VARCHAR(50) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, ended_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
