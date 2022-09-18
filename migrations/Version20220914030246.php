574bafa7049808656546b0792c9b338af320c089
ujikstark <ujikbauk@gmail.com>
2022-09-18 10:45:48 +0700

    change migrations for postgresql

--- migrations/Version20220914030246.php
+++ migrations/Version20220914030246.php
@@ -20,7 +20,7 @@ final class Version20220914030246 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE attendance (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, description VARCHAR(100) DEFAULT NULL, entered_at VARCHAR(255) DEFAULT NULL, exited_at VARCHAR(255) DEFAULT NULL, INDEX IDX_6DE30D91A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE attendance (id SERIAL NOT NULL, user_id INT NOT NULL, description VARCHAR(100) DEFAULT NULL, entered_at VARCHAR(255) DEFAULT NULL, exited_at VARCHAR(255) DEFAULT NULL, INDEX IDX_6DE30D91A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE attendance ADD CONSTRAINT FK_6DE30D91A76ED395 FOREIGN KEY (user_id) REFERENCES app_user (id)');
     }
     
